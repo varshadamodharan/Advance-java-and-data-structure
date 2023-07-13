18cd38ef26649d8a2c882d696c42531e319b65e0
varshadamodharan <117900194+varshadamodharan@users.noreply.github.com>
2023-07-13 09:26:33 +0530

    Add files via upload

---
+++ DAY 4/countdigit.java
@@ -0,0 +1,25 @@
+// JAVA Code to count number of
+// digits in an integer
+import java.io.*;
+
+public class countdigit {
+
+	static int countDigit(long n)
+	{
+		int count = 0;
+		while (n != 0) {
+			n = n / 10;
+			++count;
+		}
+		return count;
+	}
+
+	/* Driver code */
+	public static void main(String[] args)
+	{
+		long n = 345289467;
+		System.out.print("Number of digits : "
+						+ countDigit(n));
+	}
+}
+
